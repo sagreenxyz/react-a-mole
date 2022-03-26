c8bb798b3c31188efe861a57edd12c10c5a1eb9d
Scott Green <sagreenxyz@gmail.com>
2022-03-25 21:23:17 -0500

    5. setting up stubs for MoleContainer, Mole, and MoleHill components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import moleImage from './assets/images/mole.png'
 import moleHillImage from './assets/images/molehill.png'
 
+function Mole() {
+  return <img src={moleImage} alt="Mole" />
+}
+
+function MoleHill() {
+  return <img src={moleHillImage} alt="Mole Hill" />
+}
+
+function MoleContainer() {
+
+  return (
+    <>
+      <Mole />
+      <MoleHill />
+    </>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <img src={moleImage} alt="Mole" />
-      <img src={moleHillImage} alt="Mole Hill" />
+      <MoleContainer />      
     </div>
   );
 }
