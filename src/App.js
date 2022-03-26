575429a5171a82d3b91d5bee28a1af16ec19aa6e
Scott Green <sagreenxyz@gmail.com>
2022-03-25 22:11:22 -0500

    16. moving displayMole logic to MoleContainer component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import moleHillImage from './assets/images/molehill.png'
 function Mole(props) {
   // #TODO create timer to determine the lifespan of a mole
   // #TODO a useEffect() hook to start timer and clean it up afterward - does this go in the MoleContainer component?
-  return props.displayMole ? <img src={moleImage} alt="Mole" style={{width: '75px'}} onClick={props.handleClick} /> : <></>
+  return <img src={moleImage} alt="Mole" style={{width: '75px'}} onClick={props.handleClick} />
 }
 
 function MoleHill(props) {
   // #TODO a useEffect() hook to start timer and clean it up afterward - does this go in the MoleContainer component?
-  return props.displayMole ? <></> : <img src={moleHillImage} alt="Mole Hill" style={{width: '75px'}} />
+  return <img src={moleHillImage} alt="Mole Hill" style={{width: '75px'}} />
 }
 
 function MoleContainer(props) {
@@ -20,17 +20,12 @@ function MoleContainer(props) {
   const handleClick = () => {
     props.setScore(props.score + 1)
     setDisplayMole(false)
-
   }
 
-  // #TODO some logic to determine which child to render - does this go in the return below?
-
-  return (
-    <>
-      <Mole displayMole={displayMole} handleClick={handleClick}/>
-      <MoleHill displayMole={displayMole}/>
-    </>
-  )
+  const moleOrMoleHill = () => {
+    return displayMole ? <Mole displayMole={displayMole} handleClick={handleClick}/> : <MoleHill displayMole={displayMole}/>
+  }
+  return (<>{moleOrMoleHill()}</>)
 }
 
 function App(){
@@ -41,7 +36,6 @@ function App(){
       for (let i = 0; i < 9; i++) {
           hills.push(<MoleContainer key={i} setScore={setScore} score={score} />)
       }
-  
       return (<div>{hills}</div>)
   }
 
