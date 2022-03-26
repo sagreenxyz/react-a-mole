8edebff323e03a5430b16deb996d9965c9f3f6a7
Scott Green <sagreenxyz@gmail.com>
2022-03-25 21:34:52 -0500

    11. more function stubs and todos

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import moleHillImage from './assets/images/molehill.png'
 
 function Mole(props) {
   // #TODO create timer to determine the lifespan of a mole
+  // #TODO a useEffect() hook to start timer and clean it up afterward - does this go in the MoleContainer component?
   return props.displayMole === 'true' ? <img src={moleImage} alt="Mole" /> : <></>
 }
 
 function MoleHill(props) {
+  // #TODO a useEffect() hook to start timer and clean it up afterward - does this go in the MoleContainer component?
   return props.displayMole === 'true' ? <></> : <img src={moleHillImage} alt="Mole" />
 }
 
 function MoleContainer() {
   const [displayMole, setDisplayMole] = useState('false')
+
+  const handleClick = () => {
+    // #TODO increment score by 1 and immediately set the clicked-on mole to no longer display (passing prop to Mole components)
+  }
+
+  // #TODO some logic to determine which child to render - does this go in the return below?
+
   return (
     <>
       <Mole displayMole="false"/>
@@ -24,6 +33,11 @@ function MoleContainer() {
 
 function App() {
   const [score, setScore] = useState(0);
+
+  const generateMoleContainers = () => {
+    // #TODO create 9 mole containers
+  }
+
   return (
     <div className="App">
       <MoleContainer />      
